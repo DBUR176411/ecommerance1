b02c84d0190ce759edff2b18a4fc9203a41647bc
DBUR176411 <101642702+DBUR176411@users.noreply.github.com>
2022-05-15 10:57:54 +0300

    commit end

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,7 @@ use App\Models\product;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\productcontroller;
 use App\Http\Controllers\CatagoryController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,3 +30,12 @@ Route::get('/Catagory/get_by_id/{id}', [CatagoryController::class, 'get_by_id'])
 
 Route::get('/product/list', [ProductController::class, 'get_all'])->name('product/list'); 
 Route::get('/Catagory/get_all', [CatagoryController::class, 'get_all'])->name('Catagory/get_all');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
